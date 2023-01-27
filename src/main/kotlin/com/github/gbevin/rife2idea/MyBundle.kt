8a0b0fc00cfcb29a69e24d12b1916534dbf20dad
GitHub Action <action@github.com>
2023-01-27 03:13:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/gbevin/rife2idea/MyBundle.kt
+++ src/main/kotlin/com/github/gbevin/rife2idea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gbevin.rife2idea
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/gbevin/rife2idea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gbevin/rife2idea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gbevin.rife2idea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gbevin.rife2idea.MyBundle
 
 class MyApplicationService {
 
