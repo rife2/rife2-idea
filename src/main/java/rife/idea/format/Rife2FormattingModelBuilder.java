404a1d97bd870c807c8d1c98d3376717f7e88321
Geert Bevin <gbevin@uwyn.com>
2023-02-04 16:52:39 -0500

    Fixed templated language formatting not working right

--- src/main/java/rife/idea/format/Rife2FormattingModelBuilder.java
+++ src/main/java/rife/idea/format/Rife2FormattingModelBuilder.java
@@ -8,8 +8,7 @@ import com.intellij.formatting.*;
 import com.intellij.formatting.templateLanguages.*;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
-import com.intellij.psi.formatter.FormattingDocumentModelImpl;
-import com.intellij.psi.formatter.xml.HtmlPolicy;
+import com.intellij.psi.templateLanguages.SimpleTemplateLanguageFormattingModelBuilder;
 import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -27,4 +26,26 @@ public abstract class Rife2FormattingModelBuilder extends TemplateLanguageFormat
     }
 
     protected abstract IElementType getTemplateTextElementType();
+
+    /**
+     * We have to override {@link TemplateLanguageFormattingModelBuilder#createModel}
+     * since after we delegate to some templated languages, those languages (xml/html for sure, potentially others)
+     * delegate right back to us to format the HbTokenTypes.OUTER_ELEMENT_TYPE token we tell them to ignore,
+     * causing a stack-overflowing loop of polite format-delegation.
+     */
+    @Override
+    public @NotNull FormattingModel createModel(@NotNull FormattingContext formattingContext) {
+        return new SimpleTemplateLanguageFormattingModelBuilder().createModel(formattingContext);
+    }
+
+    /**
+     * Do format my model!
+     *
+     * @return false all the time to tell the {@link TemplateLanguageFormattingModelBuilder}
+     * to not-not format our model (i.e. yes please!  Format away!)
+     */
+    @Override
+    public boolean dontFormatMyModel() {
+        return false;
+    }
 }
